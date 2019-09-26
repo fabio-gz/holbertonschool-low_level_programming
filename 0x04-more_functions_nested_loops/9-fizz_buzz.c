df0555adb135e5e227de6dfc3abeec514134ccf9
nildiert <fabiogomez.silva@gmail.com>
2019-09-26 20:41:58 +0000

    new line fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0 && n % 5 == 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
@@ -26,10 +26,11 @@ int main(void)
 		{
 			printf("%i", n);
 		}
-		if (n < 100)
+		if (n != 100)
 		{
 			printf(" ");
 		}
 	}
+	printf("\n");
 	return (0);
 }
