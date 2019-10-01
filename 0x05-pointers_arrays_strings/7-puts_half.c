9f0b6b0bd123a86cf72ad5ac3ce2e641e401972c
nildiert <fabiogomez.silva@gmail.com>
2019-10-01 19:26:21 +0000

    fix on odds

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,16 +12,16 @@ void puts_half(char *str)
 		n++;
 	}
 
-	if (n % 2 == 0)
+	if ((n % 2) == 0)
 	{
 		for (i = n / 2; i < n; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
-	else if (n % 2 == 1)
+	else
 	{
-		for (i = ((n - 1) / 2); i < n; i++)
+		for (i = (n + 1) / 2; i < n; i++)
 		{
 			_putchar(str[i]);
 		}
