665ee08a95133590f16355ab047e9dd96ca4555c
nildiert <fabiogomez.silva@gmail.com>
2019-10-01 14:08:14 +0000

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *_puts - prints a string
+ *@str: character
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
