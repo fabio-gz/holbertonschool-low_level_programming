d9a7f940f8e1e97ac32aa9cc6d92cefb1cf27c20
nildiert <fabiogomez.silva@gmail.com>
2019-10-01 12:48:18 +0000

    readme and header

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Project for pointers strings and arrays
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,15 @@
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+int _putchar(char c);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
