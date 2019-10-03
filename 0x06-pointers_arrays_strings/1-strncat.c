ac3e47216a7171908681b4b7902b821bb6cfb8dc
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-03 17:35:54 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *_strncat - concatenate two strings
+ *@dest: destination
+ *@src: source
+ *@n: number of bytes
+ *Return: character with n bytes
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *str = dest;
+	int m, i;
+
+	for (; dest[m] != '\0';)
+	{
+		m++;
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
+	return str;
+}
