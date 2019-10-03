37ab380e9c3bdd2f55ba201204dc209259b03579
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-03 23:27:32 +0000

    capital string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ *cap_string - capitalizes all words
+ *@s: string
+ *Return: string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+			continue;
+		}
+		if (s[i] == ' ')
+		{
+			i++;
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+			continue;
+		}
+		else
+		{
+			if (s[i] >= 'A' && s[i] <= 'Z')
+			{
+				s[i] = s[i] + 32;
+			}
+		}
+	}
+	return (s);
+}
