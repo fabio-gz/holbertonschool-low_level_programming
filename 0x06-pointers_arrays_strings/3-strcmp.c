4ccaf3cb0dadc8c58a5210e0d54abd5310af3fed
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-03 19:39:28 +0000

    loop added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,8 +7,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int n;
-
-	n = *s1 - *s2;
-	return (n);
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
