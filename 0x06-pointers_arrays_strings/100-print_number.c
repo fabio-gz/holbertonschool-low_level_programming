e5eebae73aa5e1af9a069e425e03f44e22d179d5
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-05 00:29:27 +0000

    print integer

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *print_number - prints an integer
+ *@n: integer
+ *Return: integer
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + '0');
+}
