22c004819b0916413b18060500921fe41f7d27ee
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-07 17:15:22 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *_strspn - lenght ogf a prefix substring
+ *@s: string to be scaned
+ *@accept: string to compare
+ *Return: lenght
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+	return (i);
+}
