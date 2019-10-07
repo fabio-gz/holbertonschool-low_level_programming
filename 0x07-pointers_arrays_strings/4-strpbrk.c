04d0688da76e0dd83954ed461c508d32c29133da
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-07 21:00:43 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_strpbrk - search a string for a set of bytes
+ *@s: string
+ *@accept: string to look
+ *Return: string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				s = s + i;
+				return (s);
+			}
+		}
+	}
+	return (s);
+}
