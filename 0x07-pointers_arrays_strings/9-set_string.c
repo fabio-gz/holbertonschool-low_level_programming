a6dba06d5d79df0ff0ec1a9574a50433643a5582
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-08 03:58:39 +0000

    dereference

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *set_string - set value to a pointer to a char
+ *@s: pointer to pointer char
+ *@to: pointer to char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
