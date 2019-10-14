1eb893e4feb2918ca1ef824108d45860cf18a1ac
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-14 02:57:27 +0000

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints number of arguments
+ *@argc: count
+ *@argv: string
+ *Return: number of arguments
+ */
+int main(int argc, char *argv[])
+{
+	int n = 0;
+
+	(void) argv;
+	if (argc > n)
+	{
+		printf("%i\n", argc);
+	}
+	return (0);
+}
