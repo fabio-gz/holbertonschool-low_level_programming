24c5df09de714544909a27c9ca2667d357505709
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-17 17:03:39 +0000

    point cero

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ *malloc_checked - allocates memory
+ *@b: size of memory
+ *Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *s;
+
+	s = malloc(b);
+        if (s == NULL)
+	{
+		exit (-158);
+	}
+	return (s);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Project about More malloc, free
