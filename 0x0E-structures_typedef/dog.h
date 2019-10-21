fb87e901fdec81ee15d08f582090b94c5f9f55b8
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-21 19:29:53 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dogo
+#define dogo
+/**
+ *struct dog - types for dog
+ *@name: dogs name
+ *@age: dogs age
+ *@owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
