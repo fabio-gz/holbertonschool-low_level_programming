2fa310ae442d06052139bde8bb6e3703819fabeb
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-23 18:25:05 +0000

    adding conditional

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef _FUNCPOINT_
+#define _FUNCPOINT_
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -5,3 +7,4 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
 void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
