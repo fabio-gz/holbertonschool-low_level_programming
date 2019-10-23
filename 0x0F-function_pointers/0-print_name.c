e289e7d66c08a72bd7162870207488713d9e381a
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-23 17:02:49 +0000

    point cero

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ *print_name - prints a nem
+ *@name: string of chars
+ *@f: pointer to a function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
