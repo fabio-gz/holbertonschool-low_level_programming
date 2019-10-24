84c6d1d86e2b11f9abe39135063dd7fe300316c5
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-24 02:45:55 +0000

    math functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ *op_add - func to sum
+ *@a: int 1
+ *@b: int 2
+ *Return: sum of values
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - func to sum
+ *@a: int 1
+ *@b: int 2
+ *Return: substraction of values
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - func to multuplicate
+ *@a: int 1
+ *@b: int 2
+ *Return: product of values
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - func to divide
+ *@a: int 1
+ *@b: int 2
+ *Return: values divided
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - module function
+ *@a: int 1
+ *@b: int 2
+ *Return: module of values
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
