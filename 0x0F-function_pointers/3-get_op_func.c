fb1c74abfb9666eb28dfc5b3de4ecbd5a34bbfed
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-24 02:46:50 +0000

    pointer to a function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *get_op_func - func that get operarion functions
+ *@s: operator
+ *Return: function called
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op[0] != *s && i < 5)
+	{
+		i++;
+	}
+	return (ops[i].f);
+}
