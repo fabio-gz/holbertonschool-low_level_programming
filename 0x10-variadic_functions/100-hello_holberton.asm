c3b72f6b5a5fb78ae1edf4a27276030d0dcb5fc4
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-26 20:59:52 +0000

    fix new line command

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,10 +1,10 @@
 section .data
-	  message db "Hello, Holberton", 10
+	message db "Hello, Holberton", 10
 	section .text
 	global main
 main:
 	  mov rax, 1
 	  mov rdi, 1
 	  mov rsi, message
-	  mov rdx, 16
+	  mov rdx, 17
 	  syscall
