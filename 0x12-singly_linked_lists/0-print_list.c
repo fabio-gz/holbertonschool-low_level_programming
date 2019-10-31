0b90f47e401c1c463d615db1ce2248b0d7028057
fabio-gz <fabiogomez.silva@gmail.com>
2019-10-31 19:05:37 +0000

    [FEATURE] point cero

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *print_list - prints elements of list
+ *@h: nodes
+ *Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
