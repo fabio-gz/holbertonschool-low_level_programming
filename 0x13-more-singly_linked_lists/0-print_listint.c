ce91f97b68a65a4f26190a161bfcb477e678bf6d
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-04 22:48:04 +0000

    [FEATURE] point zero

---
+++ 0x13-more-singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *print_listint - print all elements in the list
+ *@h: pointer to node
+ *Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h= h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x13-more-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Project for More Singly lists
