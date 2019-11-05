cf10916822562df95c7b0e7cd45710d13c266829
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-05 12:04:32 +0000

    [FEATURE] free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *free_listint2 - frees a list
+ *@head: pointer to the head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head;
+	listint_t *next;
+
+	while (tmp != NULL)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+	*head = NULL;
+}
