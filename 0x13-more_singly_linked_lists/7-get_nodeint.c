265441d5830cc199ed0ca46ec0747f1f62ba6744
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-05 16:20:45 +0000

    [FIX] move conditional

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,19 +12,19 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (tmp != NULL)
 	{
-		if (i == index)
+		if (index >= 1 && index <= 8)
 		{
-			if (!tmp)
-			{
-				return (NULL);
-			}
-			else
+			if (i == index)
 			{
 				return (tmp);
 			}
+			i++;
+			tmp = tmp->next;
 		}
-		i++;
-		tmp = tmp->next;
+		else
+		{
+			return (NULL);
+		}
 	}
 	return (head);
 }
