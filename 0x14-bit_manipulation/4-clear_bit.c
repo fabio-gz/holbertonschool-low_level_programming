a5fbeb238d204728647405ef461e249c866e5d6c
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-08 00:24:39 +0000

    [FEATURE] set bit to zero

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *clear_bit - set bit to 0
+ *@n: pointer to value
+ *@index: values index
+ *Return: 1 on success, -1 if failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int m = 0;
+
+	if (index > 32)
+		return (-1);
+
+	while (m <= index)
+	{
+		*n = *n & (~1 << index);
+		m++;
+	}
+	return (1);
+}
