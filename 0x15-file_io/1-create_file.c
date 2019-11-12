80383953e58482a6b79881fc25d661fc9f94c9dc
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-12 21:58:08 +0000

    [FIX] reorder the lenght of string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,10 @@ int create_file(const char *filename, char *text_content)
 	int fd, i;
 	int n = 0;
 
-	for (; text_content[n] != '\0';)
-		n++;
-
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
@@ -24,6 +21,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
+
+	for (; text_content[n] != '\0';)
+		n++;
 
 	i = write(fd, text_content, n);
 	if (i == -1)
