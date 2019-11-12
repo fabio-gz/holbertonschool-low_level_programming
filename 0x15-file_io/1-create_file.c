903e4e7815e5a8950049e9dccf1e0a595325aa22
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-12 21:25:32 +0000

    [FIX] empty file conditional

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
 	write(fd, text_content, n);
 
 	return (1);
