a700356bc60e82e0d057009351f6e6a27e2e6346
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-12 20:55:14 +0000

    [FEATURE] create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *create_file - creates a file
+ *@filename: file to create
+ *@text_content: file content
+ *Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int n = 0;
+
+	for (; text_content[n] != '\0';)
+		n++;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		write(fd, text_content, n);
+	}
+	return (1);
+}
