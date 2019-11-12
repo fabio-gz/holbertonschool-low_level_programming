83fde34d1acf2c8c376971f6bace0a2b772980be
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-12 21:40:18 +0000

    [FIX] modify empty file conditional

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
-	if (text_content == NULL)
-		return (1);
-
 	if (fd == -1)
 		return (-1);
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
 
 	write(fd, text_content, n);
 
