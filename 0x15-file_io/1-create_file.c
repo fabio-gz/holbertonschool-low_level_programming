8cdec3b115993958e1d22787c2828482c5e5b4d5
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-12 21:06:05 +0000

    [FIX] close the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,9 +20,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		write(fd, text_content, n);
-	}
+	write(fd, text_content, n);
+
 	return (1);
+	close(fd);
 }
