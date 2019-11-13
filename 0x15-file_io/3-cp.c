fd06d16f2b83a30af72c50ba103bfe1679624170
fabio-gz <fabiogomez.silva@gmail.com>
2019-11-13 23:36:21 +0000

    [FIX] adding error conditionals and saving lines

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,43 +1,46 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  *main - copies a file
  *@argc: argument count
  *@argv:argument vector
- *Return: file copied
+ *Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int file[2];
 	char buf[1024];
-	int re1, count = 0;
+	int re, wr, cl = 0;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	if (argv[1] == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			argv[1]);
-		exit(98);
-	}
-
 	file[0] = open(argv[1], O_RDONLY);
 	if (file[0] == -1)
-		return (-1);
-
-	re1 = read(file[0], buf, sizeof(buf));
-
+	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
 	file[1] = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file[1] == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99); }
+	while ((re = read(file[0], buf, sizeof(buf))) > 0)
 	{
-		close(file[0]);
-		return (-1);
+		if (re == -1)
+		{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98); }
+		wr = write(file[1], buf, re);
+		if (wr == -1)
+		{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99); }
 	}
-	count = write(file[1], buf, re1);
-
-	return (count);
+	cl = close(file[0]);
+	if (cl == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file[0]);
+		exit(100); }
+	cl = close(file[1]);
+	if (cl == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file[1]);
+		exit(100); }
+	return (0);
 }
