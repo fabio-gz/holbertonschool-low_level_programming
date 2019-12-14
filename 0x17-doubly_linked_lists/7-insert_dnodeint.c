e4ec311a2109695ac004d841bda65042352207bd
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-14 23:19:15 +0000

    [Add] conditional for initial node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (tmp == NULL)
 			return (NULL);
 	}
+	if (idx == 0)
+	{
+		add_dnodeint(h, n);
+	}
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
