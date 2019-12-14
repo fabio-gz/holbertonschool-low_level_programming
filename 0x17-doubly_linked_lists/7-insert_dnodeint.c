b6f39f4efde35432b13de271f5c56e6871ca3cc4
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-14 22:07:36 +0000

    [Fix] modify null conditional

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,13 +11,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *tmp = *h;
 	unsigned int i = 0;
 
-	if (h == NULL)
-		return (NULL);
-
 	while (i < idx - 1)
 	{
 		tmp = tmp->next;
 		i++;
+		if (tmp == NULL)
+			return (NULL);
 	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
