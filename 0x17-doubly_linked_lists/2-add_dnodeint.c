0664ec5bd42b4cf386cbee22b85152b5892b69db
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-14 17:45:44 +0000

    [Fix] return head address

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,12 +22,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	new->next = *head;
 	*head = new;
-	if (new != NULL)
-	{
-		return (new);
-	}
-	else
-	{
-		return (NULL);
-	}
+	return (*head);
 }
