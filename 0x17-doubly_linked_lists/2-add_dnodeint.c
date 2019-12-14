757a46743c905c6a0eac9878ff39422b55432772
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-14 17:40:28 +0000

    [Fix] add null conditional

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,9 +17,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		(*head)->prev = new;
 	}
+
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
 	*head = new;
-	return (new);
+	if (new != NULL)
+	{
+		return (new);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
