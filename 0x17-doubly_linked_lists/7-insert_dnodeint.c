f5669eb8daa51ea51afb60cf07adfc7e62066ace
fabio-gz <fabiogomez.silva@gmail.com>
2019-12-14 22:00:40 +0000

    [Fix] add null conditional

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,6 +11,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *tmp = *h;
 	unsigned int i = 0;
 
+	if (h == NULL)
+		return (NULL);
+
 	while (i < idx - 1)
 	{
 		tmp = tmp->next;
