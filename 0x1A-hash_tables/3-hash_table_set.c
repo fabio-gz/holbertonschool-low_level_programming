58cc57225b4173bbe24f7fb688b3c11f0ac89610
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-19 22:20:38 +0000

    [fix]edge case

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *nde;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || ht->array == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
