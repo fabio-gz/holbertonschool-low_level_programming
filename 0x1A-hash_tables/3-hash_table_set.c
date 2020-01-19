be1ab7868b129cdc8d1dfb5a48a1f2eb947aa279
fabio-gz <fabiogomez.silva@gmail.com>
2020-01-19 22:29:17 +0000

    [fix]pointer to array

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	nde = ht->array[index];
 	while (nde != NULL)
 	{
-		if (strcmp(nde->key, key) == 0)
+		if (!strcmp(nde->key, key))
 		{
 			free(nde->value);
 			nde->value = strdup(value);
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new->key = strdup(key);
 	new->value = strdup(value);
-	new->next = nde;
+	new->next = ht->array[index];
 	ht->array[index] = new;
 
 	return (1);
